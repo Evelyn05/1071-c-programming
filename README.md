3ec2b8641b5e1fe1df3854e63af24483638dd13d
Evelyn05 <shanevelyn13@gmail.com>
2018-09-17 17:20:52 +0800

    Adding W01 files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 1071-c-programming
+
+*week01 Welcome.cpp
+*week02 tempcovert.cpp
+*week02 math.cpp
---
+++ W01/welcome.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main(){
+	printf("Welcome to C programming\n");
+	printf("My name:¡È®K¨¿ \n");
+	return 0;
+	
+}
+
