87ce9277103d2352c7ec4880f7311fc51b4ba43c
Evelyn05 <43270507+Evelyn05@users.noreply.github.com>
2018-10-09 21:43:54 +0800

    Add files via upload

--- myself/4.cpp
+++ myself/4.cpp
@@ -2,13 +2,21 @@
 
 int main(){
 
-int N,answer;
-while(){
-    printf("Enter N(-1 TO exit): ");
-    scanf("%d",&N);
-    answer=(N%1000%100/10)*1000
+int N,answer,D;
+while(N>0){
+        printf("Enter N(-1 TO exit): ");
+        scanf("%d",&N);
+if(N>0){
 
+    answer=(N%1000%100/10)*1000+(N%1000%100%10)*100+((N/1000+5)%10)*10+(N%1000/100);
+    D=(((answer%1000%100/10)+5)%10)*1000+(answer%1000%100%10)*100+(answer/1000)*10+answer%1000/100;
+    printf("encode(%d)=%d\n",N,answer);
+    printf("decode(%d)=%d\n",answer,D);
 
-
+}else{
+    printf("Bye! coding by 406530120");
 }
 }
+return 0;
+}
+
