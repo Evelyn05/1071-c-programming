9405a7610f70a8f0e4b56cb5351b8aa8e9801e69
Evelyn05 <43270507+Evelyn05@users.noreply.github.com>
2018-10-27 13:46:10 +0800

    Update 三個數字之和.cpp

--- myself/三個數字之和.cpp
+++ myself/三個數字之和.cpp
@@ -2,11 +2,11 @@
 
 int integer1,integer2,integer3,sum;
 int main(){
-    printf("Please Enter first integer \n");
+    printf("Please Enter first integer: \n");
     scanf("%d",&integer1);
-    printf("Please Enter second integer \n");
+    printf("Please Enter second integer: \n");
     scanf("%d",&integer2);
-    printf("Please Enter third integer \n");
+    printf("Please Enter third integer: \n");
     scanf("%d",&integer3);
     sum= integer1+integer2+integer3;
     printf("Sum is %d.\n",sum);
