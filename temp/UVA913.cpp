9493e84cf08bebb36d7050518ea07ecfce8e7d55
Evelyn05 <43270507+Evelyn05@users.noreply.github.com>
2018-11-20 17:19:50 +0800

    Add files via upload

---
+++ temp/UVA913.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdio.h>
+#include<cmath>
+#include<iomanip>
+#include<string.h>
+using namespace std;
+
+
+
+int main(int argc, char** argv)
+{
+	long long int n;
+	while(cin>>n)
+	{
+		cout<<(3*n*n+6*n+3)/2-9<<endl;
+	}
+
+
+
+	return 0;
+}
