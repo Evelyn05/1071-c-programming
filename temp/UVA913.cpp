6eef3695f192e5e94dde977b4c0ad2d2cee6cf14
Evelyn05 <43270507+Evelyn05@users.noreply.github.com>
2018-12-03 17:47:05 +0800

    Update UVA913.cpp

--- temp/UVA913.cpp
+++ temp/UVA913.cpp
@@ -1,12 +1,17 @@
 #include <stdio.h>
- 
+
 int main()
 {
     long long n;
-    while(scanf("%lld", &n) != EOF){
-        n = (1 + n)/2;
-        n *= n;
-        printf("%lld\n", n * 6 - 9);
-    }
+   while(scanf("%11d",&n)!=EOF){
+        long long sum=0,tnum=0;
+        for(int i=n;i>=1;i=i-2){
+            tnum+=i;
+            sum=3*(2*(tnum-1)-1);
+            printf("%11d\n",sum);
+        }
+
+
+   }
     return 0;
 }
