1dd99bc066c69b448b67862e955f2a59cc2b5562
Evelyn05 <43270507+Evelyn05@users.noreply.github.com>
2018-12-10 17:35:44 +0800

    Add files via upload

---
+++ UVA/uva 11541.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int n;
+    while(scanf("%d",&n)!=EOF){
+        getchar();
+        for(int i=1;i<=n;i++){
+            printf("Case%d:",i);
+        }
+
+    }
+
+
+return 0;
+}
