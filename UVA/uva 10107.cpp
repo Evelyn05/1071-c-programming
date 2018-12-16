2f3dafa27f1297d694a71aa39167dfae02ec7589
Evelyn05 <43270507+Evelyn05@users.noreply.github.com>
2018-12-16 14:02:01 +0800

    Update uva 10107.cpp

--- UVA/uva 10107.cpp
+++ UVA/uva 10107.cpp
@@ -1,5 +1,20 @@
-# include <stdio.h>
-int main(){
+#include <stdio.h>
 
-return 0;
+#define n 1000
+
+int main()
+{int data[n];
+	while(scanf("%d",&data[n])!=0){
+        for(int i=0;i<=n;i++){
+                if(data[i]==1)return 1;
+        else if(data[i]>data[i+1]){
+                int temp;
+                temp=data[i];
+                data[i]=data[i+1];
+                data[i+1]=temp;
+            }
+        }printf("%d\n",((n+1)%2)?(data[n/2]):((data[n/2]+data[n/2-1])/2));
+	}
+	return 0;
 }
+
