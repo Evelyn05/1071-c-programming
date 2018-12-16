b174a6141dab7858267f171226e358e98c0d5044
Evelyn05 <43270507+Evelyn05@users.noreply.github.com>
2018-12-16 11:20:06 +0800

    Update uva 256.cpp

--- UVA/uva 256.cpp
+++ UVA/uva 256.cpp
@@ -1,42 +1,51 @@
 #include <stdio.h>
-#include <string.h>
-int main() {
-	int n, i, a, b;
-	char s1[10], s2[10];
-	while(scanf("%d", &n) == 1) {
-		int l1 = 1, l2 = 1;
-		for(i = 0; i < n; i++)
-			l1 *= 10;
-		for(i = 0; i < n/2; i++)
-			l2 *= 10;
-		for(i = 0; i < l1; i++) {
-			if(i*i >= l1)	break;
-			a = i*i/l2, b = i*i%l2;
-			if(n == 2 && (a+b)*(a+b) == i*i) {
-				sprintf(s1, "%01d%01d", a, b);
-				sprintf(s2, "%02d", i*i);
-				if(strcmp(s1, s2) == 0)
-					printf("%s\n", s2);
-			}
-			if(n == 4 && (a+b)*(a+b) == i*i) {
-				sprintf(s1, "%02d%02d", a, b);
-				sprintf(s2, "%04d", i*i);
-				if(strcmp(s1, s2) == 0)
-					printf("%s\n", s2);
-			}
-			if(n == 6 && (a+b)*(a+b) == i*i) {
-				sprintf(s1, "%03d%03d", a, b);
-				sprintf(s2, "%06d", i*i);
-				if(strcmp(s1, s2) == 0)
-					printf("%s\n", s2);
-			}
-			if(n == 8 && (a+b)*(a+b) == i*i) {
-				sprintf(s1, "%04d%04d", a, b);
-				sprintf(s2, "%08d", i*i);
-				if(strcmp(s1, s2) == 0)
-					printf("%s\n", s2);
-			}
-		}
-	}
-    return 0;
+int main(){
+    int n;
+    while(scanf("%d",&n)!=EOF){
+            if(n==2){
+
+                for(int i=0;i<10;i++){
+                    for(int j=0;j<10;j++){
+                        if((i+j)*(i+j)==(i*10+j)){
+                           printf("%02d\n",(i+j)*(i+j));
+
+                           }
+
+                    }
+                }
+            }
+            if(n==4){
+                     for(int i=0;i<100;i++){
+                        for(int j=0;j<100;j++){
+                           if((i+j)*(i+j)==(((i/10)*1000)+((i%10)*100)+j)){
+                           printf("%04d\n",(i+j)*(i+j));
+                        }
+                     }
+
+            }
+
+
+
+        } if(n==6){
+                        for(int i=0;i<1000;i++){
+                            for(int j=0;j<1000;j++){
+                               if((i+j)*(i+j)==(((i/100)*100000)+(((i%100)/10)*10000)+((i%100%10)*1000)+j)){
+                               printf("%06d\n",(i+j)*(i+j));
+                            }
+                         }
+
+                }
+        }
+        if(n==8){
+                        for(int i=0;i<10000;i++){
+                            for(int j=0;j<10000;j++){
+                               if((i+j)*(i+j)==(((i/1000)*10000000)+(((i%1000)/100)*1000000)+((i%1000%100)/10*100000)+(i%1000%100%10)*10000+j)){
+                               printf("%08d\n",(i+j)*(i+j));
+                            }
+                         }
+
+                }
+        }
+    }
+return 0;
 }
