2f95c3557d97667f2252f737b61095aa46a3a135
Evelyn05 <43270507+Evelyn05@users.noreply.github.com>
2018-12-19 16:51:33 +0800

    Update main.cpp

--- project/main.cpp
+++ project/main.cpp
@@ -1,25 +1,33 @@
 #include <stdio.h>
 struct List{
-    int stunum
+    int stunum;
     char name;
-    int ISBN;
     char bookna;
+    int ISBN;
     int bordate;
     int retudate;
 
 };
 
+//int borrowtimes()
 int main(){
+    List lists;
+    int stunum[10]={01,02,03,04,05,06,07,08,09,10};
+    int ISBN[10]={101,102,103,104,105,106,107,108,109,110};
+    char bookna[10]={Charlotte’s Web,Peter Pan,The Giver,The Old Man and the Sea,Thirteen Reasons Why,The Outsiders,Number the Stars,A Wrinkle In Time,A List of Cages, The Hate U Give};
+    int bordate[10={};
+    int retudate[10]={};
     int choice;
     char bookna;
+    int times=0;
 while(choice!=5){
 
-    printf("圖書管理系統\n");
-    printf("1.全部圖書訊息\n");
-    printf("2.新增圖書訊息\n");
-    printf("3.刪除圖書訊息\n");
-    printf("4.借閱次數\n");
-    printf("5.退出\n");
+    printf("Library Management System\n");
+    printf("1.All List\n");
+    printf("2.Add List\n");
+    printf("3.Delete List\n");
+    printf("4.Borrow times\n");
+    printf("5.Exit\n");
     printf("?");
     scanf("%d", &choice);
 switch(choice){
@@ -27,9 +35,12 @@ switch(choice){
     case 2:
     case 3:
     case 4:
-        printf("請輸入書名:");
+        printf("Please Enter Bookname:");
         scanf("%s",&bookna);
-        printf("%s已被借閱%d次\n",bookna,);
+        if(bookna==List[10] ||bookna==alist){
+            times++;
+        }
+        printf("%s is already borrow by %d times.\n",bookna,times);
 
 
 
