febd189ba4bc8add1fdefeb2ba395b2b1336a161
Evelyn05 <43270507+Evelyn05@users.noreply.github.com>
2018-12-21 21:20:09 +0800

    Update uva 10107.cpp

--- UVA/uva 10107.cpp
+++ UVA/uva 10107.cpp
@@ -1,20 +1,22 @@
-#include <stdio.h>
+#include<iostream>
+#include<cstdio>
+using namespace std;
 
-#define n 1000
-
-int main()
-{int data[n];
-    int count=0;
-	while(scanf("%d",&data[count++])!=EOF){
-        //for(int i=0;i<=n;i++){
-                //if(data[i]==1)return 1;
-        else if(data[i]>data[i+1]){
-                int temp;
-                temp=data[i];
-                data[i]=data[i+1];
-                data[i+1]=temp;
-            }
-        }printf("%d\n",((n+1)%2)?(data[n/2]):((data[n/2]+data[n/2-1])/2));
-	}
-	return 0;
+int main(){
+  int N[10005], top = 0, temp;
+  while( scanf( "%d", &N[top++] ) != EOF ){
+    for( int i = 0 ; i < top ; i++ )
+      if( N[top-1] > N[i] ){
+        temp = N[top-1];
+        for( int j = top-1 ; j > i ; j-- )
+          N[j] = N[j-1];
+        N[i] = temp;
+        break;
+      }
+      if( top % 2 )
+        printf( "%d\n", N[top/2] );
+      else
+        printf( "%d\n", (N[top/2]+N[top/2-1])/2 );
+  }
+  return 0;
 }
