e1679ee0e4489a18517a1694adb5700d3b595f6f
Evelyn05 <43270507+Evelyn05@users.noreply.github.com>
2018-12-23 16:00:19 +0800

    Update p7_406530120.cpp

--- 2/p7_406530120.cpp
+++ 2/p7_406530120.cpp
@@ -1,26 +1,43 @@
-#include<stdio.h>
+#include <stdio.h>
+int getprime(int prime[], int n){
+    int vaild[1000];
+	int total = 0;
+	int i, j;
+	for(i=0;i<1000;i++){
+        vaild[i]=1;
+	}
+	for(i=2;i<1000;i++){
+        if(vaild[i]){
+            if(n/i<i)
+                break;
+            for(j=i*i;j<1000;j+=i)
+                vaild[j]=0;
+        }
+	}
+	for(i=2;i<1000;i++){
+        if(vaild[i])
+            prime[++total]=i;
+	}
+	return total;
 
-#include<conio.h>
-int getPrime(int primes[]){
-    int i=0,j;
-    for(i=0;i<=1000;i++)
-    {for(j=2;j<=i;j++)
-    if(i%j==0)
-    return 0;
-   else if(i%j!=0)
-    return j;
+}
+int main(){
+    int a,b;
+    int Prime[1000]={};
+    int total=getprime(Prime,1000);
+    while(1){
+        scanf("%d %d",&a,&b);
+        for(int i=0;i<1000;i++){
+            if(Prime[i]>b)
+                break;
+            if(a<Prime[i])
+                printf("%d " ,Prime[i]);
+        }
+        printf("\n");
     }
-    getch();
 
-    }
-int main()
-{int a,b,c;
-c=getPrime(primes[])
-    while (scanf("%d %d",&a,&b)!=EOF){
-            if(a<=getPrime==1<=b){
-                printf("%d",c);
-            }
 
-    }
 
+
+return 0;
 }
